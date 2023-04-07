6c0ba1042b8ee583189b4ddbc357f9a94cc4d1a8
SPARTACUS5329 <adityaspartacus5329@gmail.com>
2023-04-07 13:22:08 +0530

    simple form create

--- src/pages/CreateForm.tsx
+++ src/pages/CreateForm.tsx
@@ -3,16 +3,23 @@ import AddElement from "../components/AddElement";
 import { FormValue } from "../utils/types";
 import { FormContext } from "../utils/FormContext";
 import FormPreview from "../components/FormPreview";
-import { Button } from "antd";
+import { Button, Input } from "antd";
 import axios from "../config/_axios";
 
 function CreateForm() {
+	const [formName, setFormName] = useState<string>("");
 	const [currentForm, setCurrentForm] = useState<FormValue[]>([]);
 
 	const handleSubmit = async () => {
 		try {
+			if (formName === "") {
+				throw Error("Invalid form name");
+			}
 			console.log(currentForm);
-			const result = await axios.post("/create-form", currentForm);
+			const result = await axios.post("/create-form", {
+				name: formName,
+				elements: currentForm,
+			});
 			console.log(result);
 		} catch (error) {
 			console.error(error);
@@ -20,13 +27,21 @@ function CreateForm() {
 	};
 
 	return (
-		<div>
-			<FormContext.Provider value={currentForm}>
-				<AddElement setCurrentForm={setCurrentForm} />
-				<FormPreview />
-				<Button onClick={handleSubmit}>Create Form</Button>
-			</FormContext.Provider>
-		</div>
+		<FormContext.Provider value={currentForm}>
+			<Input
+				placeholder="Form Name"
+				onChange={(e: any) => {
+					e.preventDefault();
+					setFormName(e.target.value);
+				}}
+				style={{ marginBottom: "20px" }}
+			/>
+			<AddElement setCurrentForm={setCurrentForm} />
+			<FormPreview />
+			<Button onClick={handleSubmit} style={{ marginTop: "20px" }}>
+				Create Form
+			</Button>
+		</FormContext.Provider>
 	);
 }
 
